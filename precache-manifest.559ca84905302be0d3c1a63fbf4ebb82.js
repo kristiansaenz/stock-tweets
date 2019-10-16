d4860737d939626803f01a12d89dd26fab3883ff
Your Name <you@example.com>
2019-10-16 12:52:49 -0500

    Updates

---
+++ precache-manifest.559ca84905302be0d3c1a63fbf4ebb82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143f4c67c60a3b7a004188cd70f552c0",
+    "url": "/stock-tweets/index.html"
+  },
+  {
+    "revision": "1d40c56a0b5dc2e2f4ca",
+    "url": "/stock-tweets/static/css/main.26027b7b.chunk.css"
+  },
+  {
+    "revision": "a618d95ec7a2838a2c25",
+    "url": "/stock-tweets/static/js/2.0bb803e7.chunk.js"
+  },
+  {
+    "revision": "1d40c56a0b5dc2e2f4ca",
+    "url": "/stock-tweets/static/js/main.c2173ace.chunk.js"
+  },
+  {
+    "revision": "ee6d8847ea1955577342",
+    "url": "/stock-tweets/static/js/runtime-main.f89beb88.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/stock-tweets/static/media/logo.25bf045c.svg"
+  }
+]);
