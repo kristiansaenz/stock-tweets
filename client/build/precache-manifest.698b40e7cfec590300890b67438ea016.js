f536cadc5f060740e1bcdd1b79596df3250c4bf8
Your Name <you@example.com>
2019-10-16 12:52:22 -0500

    fix: changing homepage

---
+++ client/build/precache-manifest.698b40e7cfec590300890b67438ea016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f0871ec76c162a9e217a8a0cfbf813",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d40c56a0b5dc2e2f4ca",
+    "url": "./static/css/main.26027b7b.chunk.css"
+  },
+  {
+    "revision": "a618d95ec7a2838a2c25",
+    "url": "./static/js/2.0bb803e7.chunk.js"
+  },
+  {
+    "revision": "1d40c56a0b5dc2e2f4ca",
+    "url": "./static/js/main.c2173ace.chunk.js"
+  },
+  {
+    "revision": "8394354c5f2fa89a945b",
+    "url": "./static/js/runtime-main.2a0cec3b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
